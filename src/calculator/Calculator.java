75d7b45034e0b04b786302c953e957845ce0f5d2
MirelaUta <Mirela@169.254.137.236>
2018-09-08 16:47:21 +0300

    08.09.2018 - Java

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Mirela
+ */
+public class Calculator {
+    static int rezulat =0;
+    public static int aduna(int a, int b){
+    rezultat=a+b;
+    return rezultat;
+    }
+    
+    public static int scade(int a, int b){
+    rezultat=a-b;
+    return rezultat;
+    }
+    
+     public static int inmulteste(int a, int b){
+    rezultat=a*b;
+    return rezultat;
+    }
+     public static int imparte(int a, int b){
+    rezultat=a/b;
+    return rezultat;
+    }
+}
