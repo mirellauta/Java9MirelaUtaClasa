92cf39be5999731f7a234304c43586c948698dc3
MirelaUta <Mirela@169.254.137.236>
2018-09-08 16:47:21 +0300

    08.09.2018 - Java

---
+++ src/calculator/ExecutorCalcule.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Mirela
+ */
+public class ExecutorCalcule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public static int aduna(int a, int b){
+    rezultat=a+b;
+    return rezultat;
+    }
+    
+    public static int scade(int a, int b){
+    rezultat=a-b;
+    return rezultat;
+    }
+    
+     public static int inmulteste(int a, int b){
+    rezultat=a*b;
+    return rezultat;
+    }
+     public static int imparte(int a, int b){
+    rezultat=a/b;
+    return rezultat;
+    }
+}
